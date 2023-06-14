c7d1d00266005b6d5da0ffc8f69111c23b33a77b
Fornasier, Alessandro <alessandro.fornasier@aau.at>
2023-06-14 15:32:52 +0200

    [ADD] Cleanup and added camera extrinsics as parameter

--- flightgoggles_ros_bridge/src/CameraModel/FisheyeModel.cpp
+++ flightgoggles_ros_bridge/src/CameraModel/FisheyeModel.cpp
@@ -48,22 +48,21 @@ void FisheyeCamera::setCameraParameters(
     const float _fy,
     const float _cx,
     const float _cy,
-    const float _s
-    )
+    const float _s)
 {
 
-  cam_parameters_.img_width   = _img_width;
-  cam_parameters_.img_height  = _img_height;
-  cam_parameters_.fx          = _fx;
-  cam_parameters_.fy          = _fy;
-  cam_parameters_.cx          = _cx;
-  cam_parameters_.cy          = _cy;
-  cam_parameters_.s           = _s;
+  cam_parameters_.img_width = _img_width;
+  cam_parameters_.img_height = _img_height;
+  cam_parameters_.fx = _fx;
+  cam_parameters_.fy = _fy;
+  cam_parameters_.cx = _cx;
+  cam_parameters_.cy = _cy;
+  cam_parameters_.s = _s;
 
-//  std::cout << "res: " << _img_width << ", " << _img_height << "\n"
-//            << "f:   " << _fx << ", " << _fy << "\n"
-//            << "c:   " << _cx << ", " << _cy << "\n"
-//            << "s:   " << _s << std::endl;
+  //  std::cout << "res: " << _img_width << ", " << _img_height << "\n"
+  //            << "f:   " << _fx << ", " << _fy << "\n"
+  //            << "c:   " << _cx << ", " << _cy << "\n"
+  //            << "s:   " << _s << std::endl;
 
   cam_parameters_.setAdditional();
 
@@ -84,19 +83,16 @@ void FisheyeCamera::updateLookups()
   cns_ud[1] = undistortPixel(cv::Point2f(size_dis.width, 0.0));
   cns_ud[2] = undistortPixel(cv::Point2f(size_dis.width, size_dis.height));
   cns_ud[3] = undistortPixel(cv::Point2f(0.0, size_dis.height));
-//  for (uint i = 0; i < 4; ++i) {
-//    std::cout << "    Corner " << i << ": " << cns_ud[i].x << ", " << cns_ud[i].y << std::endl;
-//  }
+  //  for (uint i = 0; i < 4; ++i) {
+  //    std::cout << "    Corner " << i << ": " << cns_ud[i].x << ", " << cns_ud[i].y << std::endl;
+  //  }
 
   size_undis = cv::Size2i(
-                 std::abs(std::round(std::fmax(cns_ud[1].x, cns_ud[2].x)))
-                  + std::abs(std::round(std::fmin(cns_ud[0].x, cns_ud[3].x))),
-                 std::abs(std::round(std::fmax(cns_ud[2].y, cns_ud[3].y)))
-                  + std::abs(std::round(std::fmin(cns_ud[0].y, cns_ud[1].y)))
-      );
-//  std::cout << "  Image resolutions:\n"
-//            << "    Dis:   " << size_dis.width << ", " << size_dis.height << "\n"
-//            << "    Undis: " << size_undis.width << ", " << size_undis.height << std::endl;
+      std::abs(std::round(std::fmax(cns_ud[1].x, cns_ud[2].x))) + std::abs(std::round(std::fmin(cns_ud[0].x, cns_ud[3].x))),
+      std::abs(std::round(std::fmax(cns_ud[2].y, cns_ud[3].y))) + std::abs(std::round(std::fmin(cns_ud[0].y, cns_ud[1].y))));
+  //  std::cout << "  Image resolutions:\n"
+  //            << "    Dis:   " << size_dis.width << ", " << size_dis.height << "\n"
+  //            << "    Undis: " << size_undis.width << ", " << size_undis.height << std::endl;
 
   // set resolutions
   setCamResolution(size_dis);
@@ -111,10 +107,10 @@ void FisheyeCamera::updateLookups()
     for (int row = 0; row < size_undis.height; ++row)
     {
       cv::Point2f pix_ud = undistortPixel(cv::Point2f(col, row));
-      lookup_dis_x.at<float>(row,col) = pix_ud.x;
-      lookup_dis_y.at<float>(row,col) = pix_ud.y;
+      lookup_dis_x.at<float>(row, col) = pix_ud.x;
+      lookup_dis_y.at<float>(row, col) = pix_ud.y;
     } // for (int row = 0; row < size_undis.height; ++row)
-  } // for (int col = 0; col < size_undis.width; ++col)
+  }   // for (int col = 0; col < size_undis.width; ++col)
   setLookupDistortion(lookup_dis_x, lookup_dis_y);
 
   // create undistortion lookup
@@ -126,12 +122,11 @@ void FisheyeCamera::updateLookups()
     for (int row = 0; row < size_dis.height; ++row)
     {
       cv::Point2f pix_d = distortPixel(cv::Point2f(col, row));
-      lookup_und_x.at<float>(row,col) = pix_d.x;
-      lookup_und_y.at<float>(row,col) = pix_d.y;
+      lookup_und_x.at<float>(row, col) = pix_d.x;
+      lookup_und_y.at<float>(row, col) = pix_d.y;
     } // for (int row = 0; row < size_undis.height; ++row)
-  } // for (int col = 0; col < size_undis.width; ++col)
+  }   // for (int col = 0; col < size_undis.width; ++col)
   setLookupUndistortion(lookup_und_x, lookup_und_y);
-
 
 } // FisheyeCamera::updateLookups()
 
@@ -189,27 +184,22 @@ void FisheyeCamera::applyDistortion(
     x_min = std::abs(std::round(std::fmin(cns_d[0].x, cns_d[3].x)));
     y_min = std::abs(std::round(std::fmin(cns_d[0].y, cns_d[1].y)));
     out_res = cv::Size2i(
-                   std::abs(std::round(std::fmax(cns_d[1].x, cns_d[2].x)))
-                    - x_min,
-                   std::abs(std::round(std::fmax(cns_d[2].y, cns_d[3].y)))
-                    - y_min
-        );
+        std::abs(std::round(std::fmax(cns_d[1].x, cns_d[2].x))) - x_min,
+        std::abs(std::round(std::fmax(cns_d[2].y, cns_d[3].y))) - y_min);
   }
-
-
 
   cv::Mat img_out;
   if (_image_in.channels() == 1)
   {
     img_out = cv::Mat(out_res, CV_8UC1, cv::Scalar(0));
 
-    std::cout << "distorting output" << std::endl;
+    // std::cout << "distorting output" << std::endl;
     for (uint col = 0; col < out_res.width; ++col)
     {
       for (uint row = 0; row < out_res.height; ++row)
       {
         // calculate the exact pixel location
-        cv::Point2f pix_ud = lookupDistortion(row+y_min, col+x_min);
+        cv::Point2f pix_ud = lookupDistortion(row + y_min, col + x_min);
         cv::Point2i pix_ref(std::floor(pix_ud.x), std::floor(pix_ud.y));
         cv::Point2f pix_diff = pix_ud - cv::Point2f(pix_ref.x, pix_ref.y);
 
@@ -219,35 +209,32 @@ void FisheyeCamera::applyDistortion(
 
         // check if pixel is within dimensions
         if (ref_w >= 0 &&
-            ref_w < (int) in_res.width-1 &&
+            ref_w < (int)in_res.width - 1 &&
             ref_h >= 0 &&
-            ref_h < (int) in_res.height-1)
+            ref_h < (int)in_res.height - 1)
         {
-          img_out.at<uchar>(row,col) =
-              pix_diff.x * (pix_diff.y*_image_in.at<uchar>(ref_h+1, ref_w+1)
-                            + (1-pix_diff.y)*_image_in.at<uchar>(ref_h, ref_w+1))
-              + (1-pix_diff.x) * (pix_diff.y*_image_in.at<uchar>(ref_h+1, ref_w)
-                                  + (1-pix_diff.y)*_image_in.at<uchar>(ref_h, ref_w));
+          img_out.at<uchar>(row, col) =
+              pix_diff.x * (pix_diff.y * _image_in.at<uchar>(ref_h + 1, ref_w + 1) + (1 - pix_diff.y) * _image_in.at<uchar>(ref_h, ref_w + 1)) + (1 - pix_diff.x) * (pix_diff.y * _image_in.at<uchar>(ref_h + 1, ref_w) + (1 - pix_diff.y) * _image_in.at<uchar>(ref_h, ref_w));
         } // if
         else
         {
-          img_out.at<uchar>(row,col) = (uchar)0;
+          img_out.at<uchar>(row, col) = (uchar)0;
         } // else
-      } // for (uint col = 0; col < res.x; ++col)
-    } // for (uint row = 0; row < res.y; ++row)
+      }   // for (uint col = 0; col < res.x; ++col)
+    }     // for (uint row = 0; row < res.y; ++row)
 
   } // _image_in.channels()
   else if (in_channels == 3)
   {
     img_out = cv::Mat(out_res, CV_8UC3, cv::Scalar(0));
 
-    std::cout << "distorting output" << std::endl;
+    // std::cout << "distorting output" << std::endl;
     for (uint col = 0; col < out_res.width; ++col)
     {
       for (uint row = 0; row < out_res.height; ++row)
       {
         // calculate the exact pixel location
-        cv::Point2f pix_ud = lookupDistortion(row+y_min, col+x_min);
+        cv::Point2f pix_ud = lookupDistortion(row + y_min, col + x_min);
         cv::Point2i pix_ref(std::floor(pix_ud.x), std::floor(pix_ud.y));
         cv::Point2f pix_diff = pix_ud - cv::Point2f(pix_ref.x, pix_ref.y);
 
@@ -257,22 +244,19 @@ void FisheyeCamera::applyDistortion(
 
         // check if pixel is within dimensions
         if (ref_w >= 0 &&
-            ref_w < (int) in_res.width-1 &&
+            ref_w < (int)in_res.width - 1 &&
             ref_h >= 0 &&
-            ref_h < (int) in_res.height-1)
+            ref_h < (int)in_res.height - 1)
         {
-          img_out.at<cv::Vec3b>(row,col) =
-              pix_diff.x * (pix_diff.y*_image_in.at<cv::Vec3b>(ref_h+1, ref_w+1)
-                            + (1-pix_diff.y)*_image_in.at<cv::Vec3b>(ref_h, ref_w+1))
-              + (1-pix_diff.x) * (pix_diff.y*_image_in.at<cv::Vec3b>(ref_h+1, ref_w)
-                                  + (1-pix_diff.y)*_image_in.at<cv::Vec3b>(ref_h, ref_w));
+          img_out.at<cv::Vec3b>(row, col) =
+              pix_diff.x * (pix_diff.y * _image_in.at<cv::Vec3b>(ref_h + 1, ref_w + 1) + (1 - pix_diff.y) * _image_in.at<cv::Vec3b>(ref_h, ref_w + 1)) + (1 - pix_diff.x) * (pix_diff.y * _image_in.at<cv::Vec3b>(ref_h + 1, ref_w) + (1 - pix_diff.y) * _image_in.at<cv::Vec3b>(ref_h, ref_w));
         } // if
         else
         {
-          img_out.at<cv::Vec3b>(row,col) = 0, 0, 0;
+          img_out.at<cv::Vec3b>(row, col) = 0, 0, 0;
         } // else
-      } // for (uint col = 0; col < res.x; ++col)
-    } // for (uint row = 0; row < res.y; ++row)
+      }   // for (uint col = 0; col < res.x; ++col)
+    }     // for (uint row = 0; row < res.y; ++row)
   }
   else
   {
@@ -314,7 +298,7 @@ cv::Point2f FisheyeCamera::distortPixel(const cv::Point2f _P) const
   float P_x = _P.x / cam_parameters_.fx;
   float P_y = _P.y / cam_parameters_.fy;
 
-  float P_r = std::sqrt(P_x*P_x + P_y*P_y);
+  float P_r = std::sqrt(P_x * P_x + P_y * P_y);
   float ud_factor = 1.0;
   if (P_r > 0.001)
   {
@@ -348,7 +332,7 @@ cv::Point2f FisheyeCamera::undistortPixel(const cv::Point2f _pix) const
   float px_nx = (_pix.x - cam_parameters_.cx) / cam_parameters_.fx;
   float px_ny = (_pix.y - cam_parameters_.cy) / cam_parameters_.fy;
 
-  float px_r = std::sqrt(px_nx*px_nx + px_ny*px_ny);
+  float px_r = std::sqrt(px_nx * px_nx + px_ny * px_ny);
   float d_factor = 1.0;
   if (px_r > 0.01)
   {
--- flightgoggles_ros_bridge/src/ROSClient/ROSClient.hpp
+++ flightgoggles_ros_bridge/src/ROSClient/ROSClient.hpp
@@ -13,111 +13,111 @@
 #ifndef ROSCLIENT_HPP
 #define ROSCLIENT_HPP
 
-#include <FlightGogglesClient.hpp>
-#include <FisheyeModel.hpp>
-
-#include <iostream>
-#include <cmath>
-#include <random>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <thread>
-#include <atomic>
-#include <boost/algorithm/string.hpp>
-
-#include "ros/ros.h"
-#include <ros/time.h>
-#include <ros/service_server.h>
-#include "std_msgs/String.h"
-#include "std_msgs/Empty.h"
-#include "sensor_msgs/CameraInfo.h"
-#include "sensor_msgs/Range.h"
-#include "std_msgs/Float32.h"
-#include "flightgoggles/IRMarkerArray.h"
-#include "flightgoggles/FGParam.h"
+#include <cv_bridge/cv_bridge.h>
+#include <dynamic_reconfigure/server.h>
 #include <flightgoggles/SetTransformStamped.h>
-#include <flightgoggles/SetTransformStampedRequest.h>
-#include <flightgoggles/SetTransformStampedResponse.h>
 #include <flightgoggles/SetTransformStampedArr.h>
 #include <flightgoggles/SetTransformStampedArrRequest.h>
 #include <flightgoggles/SetTransformStampedArrResponse.h>
+#include <flightgoggles/SetTransformStampedRequest.h>
+#include <flightgoggles/SetTransformStampedResponse.h>
+#include <flightgoggles_ros_bridge/BridgeConfigConfig.h>
+#include <image_transport/image_transport.h>
+#include <ros/service_server.h>
+#include <ros/time.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
+#include <FisheyeModel.hpp>
+#include <FlightGogglesClient.hpp>
+#include <algorithm>
+#include <atomic>
+#include <boost/algorithm/string.hpp>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "flightgoggles/FGParam.h"
+#include "flightgoggles/IRMarkerArray.h"
 #include "geometry_msgs/Pose.h"
+#include "ros/ros.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "sensor_msgs/Range.h"
+#include "std_msgs/Empty.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/String.h"
 #include "tf2_msgs/TFMessage.h"
 
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-
-#include <tf2_ros/transform_listener.h>
-#include <tf2_eigen/tf2_eigen.h>
-
-#include <dynamic_reconfigure/server.h>
-#include <flightgoggles_ros_bridge/BridgeConfigConfig.h>
-
 class ROSClient {
-public:
+ public:
   /// @name Parameter struct
   struct SimParams {
-      std::string     sim_scene_name {"gen3dscene"};  //!< scene name to simulate
-      Eigen::Vector2i sim_cam_dim {640, 480};         //!< image dimension to use (only 640, 480)
-      float           sim_cam_fov {70};               //!<
+    std::string sim_scene_name{"gen3dscene"};
+    Eigen::Vector2i sim_cam_dim{640, 480};
 
-      float level_illumination {0.0};
-      float level_features {0.0};
-      float level_clutter {0.0};
-      float level_motion_blur {0.0};
+    float sim_cam_fov{70};
+    float level_illumination{0.0};
+    float level_features{0.0};
+    float level_clutter{0.0};
+    float level_motion_blur{0.0};
+    float cam_distortion{1.0};
 
-      float cam_distortion {1.0};
-      bool  cam_is_grayscale {false};
+    bool cam_is_grayscale{false};
 
-      float cam_time_delay {0.0};
-      float imu_acc_noise {0.0};
-      float imu_gyro_noise {0.0};
+    float cam_time_delay{0.0};
+    float imu_acc_noise{0.0};
+    float imu_gyro_noise{0.0};
 
-      uint64_t cam_time_delay_ns() {
-        return (uint64_t) (cam_time_delay * 1e9);
-      }
+    uint64_t cam_time_delay_ns() { return (uint64_t)(cam_time_delay * 1e9); }
   };
 
   typedef flightgoggles_ros_bridge::BridgeConfigConfig BridgeConfig_t;
   typedef dynamic_reconfigure::Server<BridgeConfig_t> ReconfServer_t;
 
   /// @name FlightGoggles interface object
-	FlightGogglesClient flightGoggles;
+  FlightGogglesClient flightGoggles;
 
   /// @name CameraModel
   FisheyeCamera camera_model_;
 
-	/// @name Node handles for ROS
-	//@{
-	ros::NodeHandle ns_;
+  /// @name Node handles for ROS
+  //@{
+  ros::NodeHandle ns_;
   ros::NodeHandle nsPrivate_;
   //@}
 
-	/// @name Transform listeners for listening to ROS TF
-	//@{
-	tf2_ros::Buffer tfBuffer_;
-	tf2_ros::TransformListener tfListener_;
-	//@}
+  /// @name Transform listeners for listening to ROS TF
+  //@{
+  tf2_ros::Buffer tfBuffer_;
+  tf2_ros::TransformListener tfListener_;
+  //@}
 
-	/// @name Subscribers
-	//@{
+  /// @name Transform broadcaster
+  //@{
+  tf2_ros::StaticTransformBroadcaster tfBroadcaster_;
+  //@}
+
+  /// @name Subscribers
+  //@{
   ros::Subscriber tfSubscriber_;
   ros::Subscriber irSubscriber_;
   ros::Subscriber paramSubscriber_;
+  ros::Subscriber camExtrinsicsSubscriber_;
   //@}
 
   /// @name Image publishers
   //@{
-	image_transport::ImageTransport it_;
-	image_transport::CameraPublisher imagePubLeft_;
-	image_transport::CameraPublisher imagePubRight_;
-	//@}
+  image_transport::ImageTransport it_;
+  image_transport::CameraPublisher imagePub_;
+  //@}
 
-	/// @name Topic publishers
-	//@{
-	ros::Publisher collisionPub_;
+  /// @name Topic publishers
+  //@{
+  ros::Publisher collisionPub_;
   ros::Publisher lidarPub_;
   ros::Publisher irMarkerPub_;
   ros::Publisher fpsPublisher_;
@@ -134,64 +134,113 @@ public:
   bool render_stereo = false;
   int numSimulationStepsSinceLastRender_ = 0;
   const int numSimulationStepsBeforeRenderRequest_ = 15;
-//    sensor_msgs::PointCloud2::Ptr irBeaconGroundTruth_;
-  sensor_msgs::CameraInfo cameraInfoLeft;
-  sensor_msgs::CameraInfo cameraInfoRight;
-	float baseline_ = 0.32;
-	int imageWidth_ = 1024;
-	int imageHeight_ = 768;
+  // sensor_msgs::PointCloud2::Ptr irBeaconGroundTruth_;
+  sensor_msgs::CameraInfo cameraInfo_;
+  int imageWidth_ = 1024;
+  int imageHeight_ = 768;
 
   // Sim Params
   SimParams sim_parameters_;
-  bool b_have_renders_ {false};
+  bool b_have_renders_{false};
 
   // Scene Params
   ReconfServer_t reconf_server_;
   unity_outgoing::Scene_t scene_params_;
 
-	// Lidar params
-  float lidarMaxRange_ = 20; // Meters
-  float lidarVariance_ = 0.0009; // Meters^2
+  // Lidar params
+  float lidarMaxRange_ = 20;      // Meters
+  float lidarVariance_ = 0.0009;  // Meters^2
 
   // Noise generators for lidar
   std::default_random_engine randomNumberGenerator_;
-  std::normal_distribution<float> standardNormalDistribution_ = std::normal_distribution<float>(0.0,1.0);
+  std::normal_distribution<float> standardNormalDistribution_ =
+      std::normal_distribution<float>(0.0, 1.0);
 
   // Keep track of the frame rate
   ros::WallTime timeSinceLastMeasure_;
   int64_t frameCount_;
   //@}
 
-  /// @name Transforms
-  //@{
-  geometry_msgs::TransformStamped imu_T_Camera_;
-  //@}
+  std::atomic<bool> bIamgeReceived_;
 
-  /// @name Constructor
-	ROSClient(ros::NodeHandle nh, ros::NodeHandle nhPrivate);
+  /**
+   * @brief ROSClient
+   *
+   * @param nh
+   * @param nhPrivate
+   */
+  ROSClient(ros::NodeHandle nh, ros::NodeHandle nhPrivate);
 
-	// Populate starting settings into state
-	void populateRenderSettings();
+  /**
+   * @brief Populate the render settings
+   *
+   */
+  void populateRenderSettings();
   void populateSimulationSettings();
 
+  // void irBeaconPointcloudCallback(sensor_msgs::PointCloud2::Ptr msg);
 
-	/// @name Callbacks
-	void tfCallback(tf2_msgs::TFMessage::Ptr msg);
-//    void irBeaconPointcloudCallback(sensor_msgs::PointCloud2::Ptr msg);
-  void paramCallback(const flightgoggles::FGParamConstPtr& msg);
+  /**
+   * @brief Parameters callback
+   *
+   * @param msg
+   */
+  void paramCallback(const flightgoggles::FGParamConstPtr &msg);
+
+  /**
+   * @brief Dynamic reconfigure callback
+   *
+   * @param config
+   * @param level
+   */
   void dynamicCallback(BridgeConfig_t &config, uint32_t level);
 
+  /**
+   * @brief Image distortion and color
+   *
+   * @param msg
+   * @param image
+   */
+  void imageDistortColor(sensor_msgs::ImagePtr &msg, cv::Mat image);
 
-  /// @name Image Transform Methods
+  /**
+   * @brief Callback from tf
+   *
+   * @param msg
+   */
+  void tfCallback(tf2_msgs::TFMessage::Ptr msg);
 
-  //
-  void imageDistortColor(sensor_msgs::ImagePtr& msg, cv::Mat image);
+  void camExtrinsicsCallback(const geometry_msgs::TransformStampedPtr &msg);
 
-  std::atomic<bool> bIamgeReceived_;
-  bool setTfSrvCallback(flightgoggles::SetTransformStampedRequest & request, flightgoggles::SetTransformStampedResponse& response);
-  bool setTranformStampedArrSrvCallback(flightgoggles::SetTransformStampedArrRequest & request, flightgoggles::SetTransformStampedArrResponse& response);
-private:
-  void performCameraTest();
+  /**
+   * @brief Callback from tf service
+   *
+   * @param request
+   * @param response
+   * @return true
+   * @return false
+   */
+  bool setTfSrvCallback(flightgoggles::SetTransformStampedRequest &request,
+                        flightgoggles::SetTransformStampedResponse &response);
+
+  /**
+   * @brief Callback from transform stamped service
+   *
+   * @param request
+   * @param response
+   * @return true
+   * @return false
+   */
+  bool setTranformStampedArrSrvCallback(
+      flightgoggles::SetTransformStampedArrRequest &request,
+      flightgoggles::SetTransformStampedArrResponse &response);
+
+ private:
+  /**
+   * @brief Camera imu transform
+   *
+   */
+  geometry_msgs::TransformStamped CamTransform();
 };
 
 #endif
